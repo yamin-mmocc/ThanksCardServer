d5da5b4a84d4e75aa0a0ac64fd3e66ed504114de
yamin-mmocc <yamin.mmocc@gmail.com>
2020-09-01 21:39:22 +0630

    add dataaccessprovider

---
+++ ThanksCardServer/DataAccess/DataAccessProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThanksCardServer.Model;
+
+namespace ThanksCardServer.DataAccess
+{
+    public class DataAccessProvider : IDataAccessProvider
+    {
+        private readonly ApplicationContext _context;
+
+        public DataAccessProvider(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public void AddDepartmentRecord(Departments dept)
+        {
+            _context.Departments.Add(dept);
+            _context.SaveC();
+        }
+
+        public void UpdateDepartmentRecord(Departments dept)
+        {
+            _context.Departments.Update(dept);
+            _context.SaveChanges();
+        }
+
+        public void DeleteDepartmentRecord(long id)
+        {
+            var entity = _context.Departments.FirstOrDefault(t => t.id == id);
+            _context.Departments.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public Departments GetDepartmentSingleRecord(long id)
+        {
+            return _context.Departments.FirstOrDefault(t => t.id == id);
+        }
+
+        public List<Departments> GetPatientRecords()
+        {
+            return _context.Departments.ToList();
+        }
+    }
+}
+
+    
+
---
+++ ThanksCardServer/DataAccess/IDataAccessProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThanksCardServer.Model;
+namespace ThanksCardServer.DataAccess
+{
+    interface IDataAccessProvider
+    {
+        void AddDepartmentRecord(Departments dept);
+        void UpdateDepartmentRecord(Departments dept);
+        void DeleteDepartmentRecord(long id);
+        Departments GetDepartmentSingleRecord(long id);
+        List<Departments> GetPatientRecords();
+    }
+}
